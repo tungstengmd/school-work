f5369920500aa27452030f0cfcad54a45619d3ca
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-11-26 12:09:52 +0000

    first if statement :D

--- Y9binsearch/Program.cs
+++ Y9binsearch/Program.cs
@@ -6,7 +6,11 @@
         {
             Console.WriteLine("What's your name?");
             string myString = Console.ReadLine();
-            Console.WriteLine("Hello, " + myString + "!");
+            Console.WriteLine("Hello, " + myString + "! Have a nice day!");
+            if (myString == "World")
+            {
+                Console.WriteLine("Hello, world!");
+            }
         }
     }
 }
