7f91b5321aae716b502cd167076e15e402cda090
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-11-26 11:55:58 +0000

    Change CRLF to LF

--- Y9binsearch/Program.cs
+++ Y9binsearch/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿namespace y9binsearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
