038cd7937c9f2e39433be523320749725162163d
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-11-27 12:19:26 +0000

    this should work :3

--- Y9binsearch/Program.cs
+++ Y9binsearch/Program.cs
@@ -7,15 +7,15 @@
             int[] ints = { 1, 3, 6, 8, 9, 11, 12, 16 };
             var hpp = ints.Length;
             var lpp = 0;
-            var mp = (hpp + lpp) / 2;
+            int mp = (hpp + lpp) / 2;
             Console.Write("Enter search key: ");
             int key = Convert.ToInt32(value: Console.ReadLine());
             if (key > ints[mp])
             {
                 lpp = mp + 1;
-                mp = (hpp + lpp) / 2;
+                mp = (int)Decimal.Round((hpp + lpp) / 2);
             }
-            else if (key < ints[mp]) 
+            else if (key < ints[mp])
             {
                 hpp = mp - 1;
                 mp = (hpp + lpp) / 2;
@@ -23,6 +23,9 @@
             else if (key == ints[mp])
             {
                 Console.WriteLine("Found!");
+            }
+            else {
+                Console.WriteLine("Not found. :(");
             }
         }
     }
