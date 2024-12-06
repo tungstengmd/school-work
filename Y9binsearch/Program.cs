82e499642002629c3d5fd4f4cb71a7e7d95d49e2
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-12-06 14:52:34 +0000

    Changes galore

--- Y9binsearch/Program.cs
+++ Y9binsearch/Program.cs
@@ -1,32 +1,28 @@
-﻿namespace y9binsearch
+﻿using System.Numerics;
+int[] ints = [1, 3, 6, 8, 9, 11, 12, 16];
+int hpp = ints.Length;
+int lpp = 0;
+int mp = (hpp + lpp) / 2;
+Console.Write("Enter search key: ");
+int key = Convert.ToInt32(value: Console.ReadLine());
+while (key != ints[mp])
 {
-    internal class Program
+    if (key > ints[mp])
     {
-        static void Main(string[] args)
-        {
-            int[] ints = { 1, 3, 6, 8, 9, 11, 12, 16 };
-            var hpp = ints.Length;
-            var lpp = 0;
-            int mp = (hpp + lpp) / 2;
-            Console.Write("Enter search key: ");
-            int key = Convert.ToInt32(value: Console.ReadLine());
-            if (key > ints[mp])
-            {
-                lpp = mp + 1;
-                mp = (int)Decimal.Round((hpp + lpp) / 2);
-            }
-            else if (key < ints[mp])
-            {
-                hpp = mp - 1;
-                mp = (hpp + lpp) / 2;
-            }
-            else if (key == ints[mp])
-            {
-                Console.WriteLine("Found!");
-            }
-            else {
-                Console.WriteLine("Not found. :(");
-            }
-        }
+        lpp = mp + 1;
+        mp = (hpp + lpp) / 2;
     }
+    else if (key < ints[mp])
+    {
+        hpp = mp - 1;
+        mp = (hpp + lpp) / 2;
+    }
+    else if (lpp > hpp)
+    {
+        Console.WriteLine("Not found. :(");
+    }
+}
+if (key == ints[mp])
+{
+    Console.WriteLine("Found!");
 }
